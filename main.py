dbec0d2620480ed5e8c758eb2ea098720223a4ae
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 18:55:23 -0400

    44

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from resources import *
 import tkinter as tk
 from tech import *
+from dev import *
 root = tk.Tk()
 root.title("Name of our Game")
 
@@ -23,19 +24,23 @@ def dialogue_pop_up(new_dialogue):
     dialogue3_display = tk.Label(root, text=dialogue3).pack(side=tk.RIGHT, pady=5)
 
 #Berries
+def add_something(item, variable):
+    getamount(item, 1)
+    variable.config(text=f"{item.proper()}: {getamount(item)}")
+    variable.config(state="normal")
 def berry_gather():
     berries_button.config(state="disabled")
+    berries_button.after(5000, add_something("berries", berries_button))
     berries_counter.config(text=f"Berries: {getamount("berries")}")
-    berries_button.after(5000, changeamount("berries",1))
-    berries_button.config(state="normal")
 berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
 berries_button.place(x=0,y=100)
 berries_counter = tk.Label(root, text = "Berries: 0")
 berries_counter.place(x=500,y=100)
 def brainstorm():
     brainstorm_button.config(state="disabled")
-    brainstorm_button.config(text=f"Science: {getamount("science")}")
-    brainstorm_button.after(30000, lambda: brainstorm_button.config(state="normal"), changeamount("Science",1))
+    brainstorm_button.after(30000, lambda: brainstorm_button.config(state="normal"))
+    changeamount("science",1)
+    science_counter.config(text=f"Science: {getamount("science")}")
 brainstorm_button = tk.Button(root, text="Brainstorm", command=brainstorm)
 brainstorm_button.place(x=0,y=500)
 science_counter = tk.Label(root, text = "Science: 0")
@@ -46,3 +51,4 @@ Right now, you can only get berries for food, and you need to eat to survive."""
 
 
 root.mainloop()
+devkey()
