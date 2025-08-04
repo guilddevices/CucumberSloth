cbb94e292f19b2df28cc14d782a5cbb12f209cab
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 11:43:44 -0400

    h

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import sys 
-from resources import *
+#yesfrom resources import *
 global waitnumber
 waitnumber = 5
 whattodo = input("""You are in Middle of Nowhere.
@@ -9,7 +9,7 @@ Your gatherer can gather food.
 Would you like your gatherer to gather berries?
 """)
 if whattodo.lower() == "yes":
-    for x in range(5):
+    for i in range(waitnumber, 0, -1):
         time.sleep(1)
         print(f"Wating: {waitnumber}", end="/r", flush=True)
-print(f"Berries: {getamount(food)}")
+#print(f"Berries: {getamount(berries)}")
