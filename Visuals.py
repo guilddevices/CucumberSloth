2866b4337213ffdfa7232162cf1292f783d80ea8
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 15:43:40 -0400

    e

--- Visuals.py
+++ Visuals.py
@@ -3,11 +3,11 @@ from resources import *
 root = tk.Tk()
 root.title("Name of our Game")
 #Berries
-berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
-berries_button.place(x=0,y=root.w_infoscreenheight())
-berries_counter = tk.Label(root, text = "Berries: 0")
-berries_button.place(x=100,y=root.w_infoscreenheight())
 def berry_gather():
-    changeamount(berries,1)
-    berries_counter.config(text=f"Berries: {getamount(berries)}")
+    changeamount("berries",1)
+    berries_counter.config(text=f"Berries: {getamount("berries")}")
+berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
+berries_button.place(x=0,y=100)
+berries_counter = tk.Label(root, text = "Berries: 0")
+berries_counter.place(x=100,y=100)
 root.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import time
 import sys
 from resources import *
+import tkinter as tk
+root = tk.Tk()
+root.title("Name of our Game")
+#Berries
+def berry_gather():
+    changeamount("berries",1)
+    berries_counter.config(text=f"Berries: {getamount("berries")}")
+berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
+berries_button.place(x=0,y=100)
+berries_counter = tk.Label(root, text = "Berries: 0")
+berries_counter.place(x=100,y=100)
+root.mainloop()
 global waitnumber
 waitnumber = 5
 whattodo = input("""You are in Middle of Nowhere.
