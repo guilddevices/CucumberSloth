1b1278d8049c86d5ca7a4a6074ea0edeed7d1cd1
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 15:23:21 -0400

    Berries Button

--- Visuals.py
+++ Visuals.py
@@ -1,3 +1,8 @@
 import tkinter as tk
-#from resources import *
-tk._test()
+from resources import *
+root = tk.Tk()
+root.title("Name of our Game")
+def gather_berries:
+    changeamount(berries,1)
+berries_button = tk.Button(root, text="Gather Berries", command=gather_berries)
+beries_button.place(X=-50,y=100)
