4a5010ea12cb76d80e8e856d80b1340774374116
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 16:09:33 -0400

    Added Science Lumberjack Job

--- tech.py
+++ tech.py
@@ -2,9 +2,21 @@ from resources import *
 
 science_tree = {
     "lumberjack": {
-        "berries": 
+        "Science": 2
     }
+}
+have_science = {
+    "lumberjack": False
 }
 
 def research(item):
+    for resource in science_tree[item]:
+        if science_tree[item][resource] < getamount(resource):
+            return("Not Enough Resources")
+    for resource in science_tree[item]:
+        changeamount(resource,-science_tree[item][resource])
+    have_science[item] = True
+    return "Succesfully Bought"
+
+    
 
