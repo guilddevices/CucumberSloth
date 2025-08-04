bc48ac1fc9f966ff25b4f45c6777d23e54a80dd3
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 16:46:09 -0400

    k

--- tech.py
+++ tech.py
@@ -15,11 +15,11 @@ def research(item):
     if item in science_tree:
         for resource in science_tree[item]:
             if science_tree[item][resource] < getamount(resource):
-                dialogue("Not enough resources!")
+                adddialogue("Not enough resources!")
         for resource in science_tree[item]:
             changeamount(resource,-science_tree[item][resource])
         have_science[item] = True
         update()
-        dialogue(f"Succesfully researched {item}")
+        adddialogue(f"Succesfully researched {item}")
     else:
         return "ResearchError"
