515e44c726a466f858a4f7a2c81016371e713c56
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 17:00:49 -0400

    e

--- dialogue_handling.py
+++ dialogue_handling.py
@@ -1,4 +1,3 @@
-from main import *
 import tkinter as tk
 
 dialogue1 = ""
@@ -9,7 +8,7 @@ def dialogue_pop_up(new_dialogue):
     dialogue1 = new_dialogue
     dialogue2 = dialogue1
     dialogue3 = dialogue2
-    dialogue1_display = tk.Label(root,text=dialogue1).pack(side=tk.RIGHT, pady=5)
-    dialogue2_display = tk.Label(root,text=dialogue2).pack(side=tk.RIGHT, pady=5)
-    dialogue3_display = tk.Label(root,text=dialogue3).pack(side=tk.RIGHT, pady=5)
+    dialogue1_display = tk.Label(root, text=dialogue1).pack(side=tk.RIGHT, pady=5)
+    dialogue2_display = tk.Label(root, text=dialogue2).pack(side=tk.RIGHT, pady=5)
+    dialogue3_display = tk.Label(root, text=dialogue3).pack(side=tk.RIGHT, pady=5)
 
