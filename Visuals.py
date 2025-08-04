a13c91e74ed77c736ac2411ddf8d03438a89f184
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 15:37:16 -0400

    Stuff

--- Visuals.py
+++ Visuals.py
@@ -2,8 +2,12 @@ import tkinter as tk
 from resources import *
 root = tk.Tk()
 root.title("Name of our Game")
-def gather_berries():
+#Berries
+berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
+berries_button.place(x=0,y=root.w_infoscreenheight())
+berries_counter = tk.Label(root, text = "Berries: 0")
+berries_button.place(x=100,y=root.w_infoscreenheight())
+def berry_gather():
     changeamount(berries,1)
-berries_button = tk.Button(root, text="Gather Berries", command=gather_berries)
-berries_button.place(x=0,y=130)
+    berries_counter.config(text=f"Berries: {getamount(berries)}")
 root.mainloop()
