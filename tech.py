e25d56fa9cbbef74e715daa59e572bf9d03cea56
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 16:37:40 -0400

    Added Failsafe and incorporation of dialogue

--- tech.py
+++ tech.py
@@ -12,11 +12,14 @@ have_science = {
 }
 
 def research(item):
-    for resource in science_tree[item]:
-        if science_tree[item][resource] < getamount(resource):
-            return("Not Enough Resources")
-    for resource in science_tree[item]:
-        changeamount(resource,-science_tree[item][resource])
-    have_science[item] = True
-    update()
-    return "Succesfully Bought"
+    if item in science_tree:
+        for resource in science_tree[item]:
+            if science_tree[item][resource] < getamount(resource):
+                dialogue("Not enough resources!")
+        for resource in science_tree[item]:
+            changeamount(resource,-science_tree[item][resource])
+        have_science[item] = True
+        update()
+        dialogue(f"Succesfully researched {item}")
+    else:
+        return "ResearchError"
