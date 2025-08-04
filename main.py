809070c319c323f86817fec17757c76a870e5cee
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 19:15:56 -0400

    ee

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ def dialogue_pop_up(new_dialogue):
 
 def add_something(item, button, counter):
     changeamount(item, 1)
+    counter.config(text=f"{item.capitalize()}: {getamount(item)}")
     button.config(state="normal")
-    counter.config(text=f"{item.proper()}: {getamount(item)}")
 #Berries
 def berry_gather():
     berries_button.config(state="disabled")
     berries_button.after(5000, add_something("berries", berries_button, berries_counter))
-    berries_counter.config(text=f"Berries: {getamount("berries")}")
+    update()
 berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
 berries_button.place(x=0,y=100)
 berries_counter = tk.Label(root, text = "Berries: 0")
@@ -41,7 +41,7 @@ def brainstorm():
     brainstorm_button.config(state="disabled")
     brainstorm_button.after(30000, lambda: brainstorm_button.config(state="normal"))
     changeamount("science",1)
-    science_counter.config(text=f"Science: {getamount("science")}")
+    update()
 brainstorm_button = tk.Button(root, text="Brainstorm", command=brainstorm)
 brainstorm_button.place(x=0,y=500)
 science_counter = tk.Label(root, text = "Science: 0")
@@ -50,6 +50,5 @@ science_counter.place(x=100,y=100)
 dialogue_pop_up("""You are in Middle of Nowhere.
 Right now, you can only get berries for food, and you need to eat to survive.""")
 
-
+root.after(500,devkey)
 root.mainloop()
-devkey()
