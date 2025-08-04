3982cdf8e1589e5307b25f3da5bd11e6d25a0f0f
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 15:28:42 -0400

    Changed Location

--- Visuals.py
+++ Visuals.py
@@ -5,5 +5,5 @@ root.title("Name of our Game")
 def gather_berries():
     changeamount(berries,1)
 berries_button = tk.Button(root, text="Gather Berries", command=gather_berries)
-berries_button.place(x=-50,y=100)
+berries_button.place(x=-30,y=130)
 root.mainloop()
