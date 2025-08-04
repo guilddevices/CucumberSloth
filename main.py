a1e867883d2dfa4d93db52eb2006cb453a666ffa
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 16:15:45 -0400

    d

--- main.py
+++ main.py
@@ -2,33 +2,30 @@ import time
 import sys
 from resources import *
 import tkinter as tk
+from tech import research
 root = tk.Tk()
 root.title("Name of our Game")
 #Berries
 def berry_gather():
-    changeamount("berries",1)
+    berries_button.config(state="disabled")
     berries_counter.config(text=f"Berries: {getamount("berries")}")
+    berries_button.after(5000, lambda: berries_button.config(state="normal"))
+    changeamount("berries",1)
+berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
+berries_button.place(x=0,y=100)
+berries_counter = tk.Label(root, text = "Berries: 0")
+berries_counter.place(x=500,y=100)
+def brainstorm():
+    brainstorm.config(state="disabled")
+    brainstorn.config(text=f"Science: {getamount("berries")}")
+    brainstorm.after(30000, lambda: brainstorm.config(state="normal"))
+    changeamount("brainstorm",1)
 berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
 berries_button.place(x=0,y=100)
 berries_counter = tk.Label(root, text = "Berries: 0")
 berries_counter.place(x=500,y=100)
 #Dialogue_Section
 dialogue = tk.Label(root,text=("""You are in Middle of Nowhere.
-You can get berries to eat, and you need berries"""))
+Right now, you can only get berries for food, and you need to eat to survive."""))
 dialogue.place(x=1000,y=100)
 root.mainloop()
-global waitnumber
-waitnumber = 5
-whattodo = input("""You are in Middle of Nowhere.
-You have 1 gatherer.
-Your gatherer can gather berries for food.
-Would you like your gatherer to gather berries?
-""")
-if whattodo.lower() == "yes":
-    for x in range(waitnumber):
-        print(f"Waiting: {waitnumber}", end="\r", flush=True)
-        time.sleep(1)
-        waitnumber -= 1
-    changeamount("berries", 2)
-    print(f"Berries: {getamount("berries")}")
-whattodo = input("""You need to pay your gatherer in 5 berries """)
