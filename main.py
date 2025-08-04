46c7eb9aa9de6f33b55a65f395dfc2ef75518c8e
okapidragon <ezragourevitch8@gmail.com>
2025-08-04 11:49:54 -0400

    k

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ if whattodo.lower() == "yes":
         print(f"Waiting: {waitnumber}", end="\r", flush=True)
         time.sleep(1)
         waitnumber -= 1
-print(f"Berries: {getamount(berries)}")
+print(f"Berries: {resources.getamount(berries)}")
