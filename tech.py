b5a04de6ec355374d69982691acdffabf7fabbc6
zacksloth5 <zackgourevitch@gmail.com>
2025-08-04 18:02:21 -0400

    More SCIENCE

--- tech.py
+++ tech.py
@@ -2,8 +2,13 @@ from resources import *
 from main import *
 
 science_tree = {
-    "lumberjack": {
-        "Science": 2
+    "foraging": {
+        "description": "Foraging unlocks the replacement of gathering with foraging. It does the same thing except has a chancce to give other food.",
+        "science": 2
+    },
+    "wood collecting" :{
+        "description": "Wood Collecting unlocks the collect wood button giving you wood.",
+        "science": 2
     }
 }
 have_science = {
@@ -13,10 +18,12 @@ have_science = {
 def research(item):
     if item in science_tree:
         for resource in science_tree[item]:
-            if science_tree[item][resource] < getamount(resource):
-                dialogue_pop_up("Not enough resources!")
+            if resource != "description":
+                if science_tree[item][resource] < getamount(resource):
+                    dialogue_pop_up("Not enough resources!")
         for resource in science_tree[item]:
-            changeamount(resource,-science_tree[item][resource])
+            if resource != "description":
+                changeamount(resource,-science_tree[item][resource])
         have_science[item] = True
         update()
         dialogue_pop_up(f"Succesfully researched {item}")
