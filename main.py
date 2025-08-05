291b2f5c59699d5e6259d1130517f53d8b3419a5
zacksloth5 <zackgourevitch@gmail.com>
2025-08-05 10:38:58 -0400

    d

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-global berries_button
-global science_counter
-global berries_counter
-global brainstorm_button
 import tkinter as tk
-from ourgameresources import *
 from tech import *
 from dev import *
+from gui import *
 root = tk.Tk()
 root.title("Name of our Game")
 
