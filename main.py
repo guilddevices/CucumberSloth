572aab0b8a155baf95c355abc3b0322f6f81da7d
zacksloth5 <zackgourevitch@gmail.com>
2025-08-05 15:57:32 -0400

    d

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from gui import *
-
+from tech import *
+from user import *
+import time
 #variable setting
 initialize()
 dialogue_pop_up("""You are in Middle of Nowhere.
 Right now, you can only get berries for food, and you need to eat to survive.""")
-#dialogue_pop_up("hdwjhjdhwjhwdjhwd")
+def frame():
+    update()
+    eatclock -= 1
+    if eatclock == 0:
+        eat()
+    time.sleep(1/60)
+while True do:
+    frame()
 
 #gameloop
 
---
+++ user.py
@@ -0,0 +1,18 @@
+from ourgameresources import *
+eatclock = 1200
+def eat():
+    if getamount("berries") > 0:
+        changeamount("berries",-1)
+        eatclock = 12000
+        return True
+    elif getamount("vegetables") > 0:
+        changeamount("vegetables",-1)
+        eatclock = 2400
+        return True
+    elif getamount("fruits") > 0:
+        changeamount("fruits",-1)
+        eatclock = 4800
+        return True
+    else:
+        return False
+        
