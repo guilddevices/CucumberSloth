becbc25c6b341035292c75c2ec07964d3685a5eb
zacksloth5 <zackgourevitch@gmail.com>
2025-08-05 15:05:01 -0400

    d

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from gui import *
 initialize()
 dialogue_pop_up("""You are in Middle of Nowhere.
 Right now, you can only get berries for food, and you need to eat to survive.""")
+dialogue_pop_up("hdwjhjdhwjhwdjhwd")
 
 #gameloop
 
