f4efc83641e736ad02a5453f956bebec016b1f19
okapidragon <ezragourevitch8@gmail.com>
2025-08-05 11:16:12 -0400

    kjkjkj

--- dialogue.py
+++ dialogue.py
@@ -1,5 +1,9 @@
 dialogue = {
 "Start": """You are in Middle of Nowhere.
 Right now, you can only get berries for food, and you need to eat to survive.""",
-
+"Berries": {
+"5": "You walk around and find some berries.",
+"3": "You pick some berries from a nearby bush.",
+"4": "You gather berries after finding a berry bush behind a tree."
+}
 }
