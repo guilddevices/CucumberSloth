601b49b6b89b7928c7fcefd80f4e0d1dbef9af5a
zacksloth5 <zackgourevitch@gmail.com>
2025-08-05 11:46:21 -0400

    d

--- gui.py
+++ gui.py
@@ -5,13 +5,13 @@ dialogue1 = ""
 dialogue2 = ""
 dialogue3 = ""
 def devkey():
-        key = input("What is the key? ")
-        if key != "~":
-            return
-        else:
-            for x in resourcedictionary:
-                changeamount(x,10000)
-            update()
+    key = input("What is the key? ")
+    if key != "~":
+        return
+    else:
+        for x in resourcedictionary:
+            changeamount(x,10000)
+        update()
 
 
 def update():
@@ -19,25 +19,30 @@ def update():
     science_counter.config(text=f"Science: {getamount("science")}")
 
 def dialogue_pop_up(new_dialogue):
-        dialogue3 = dialogue2
-        dialogue2 = dialogue1
-        dialogue1 = new_dialogue
-        dialogue1_display = tk.Label(root, text=dialogue1,width=20,wraplength=100).place(relx=1.0, rely=0.0, anchor="ne").pack(side=tk.TOP, pady=5)
-        dialogue2_display = tk.Label(root, text=dialogue2,width=20,wraplength=100).place(relx=1.0, rely=0.0, anchor="ne").pack(side=tk.TOP, pady=5)
-        dialogue3_display = tk.Label(root, text=dialogue3,width=20,wraplength=100).place(relx=1.0, rely=0.0, anchor="ne").pack(side=tk.TOP, pady=5)
+    dialogue3 = dialogue2
+    dialogue2 = dialogue1
+    dialogue1 = new_dialogue
+    dialogue1_display = tk.Label(root, text=dialogue1,width=20,wraplength=100).place(relx=1.0, rely=0.0, anchor="ne").pack(side=tk.TOP, pady=5)
+    dialogue2_display = tk.Label(root, text=dialogue2,width=20,wraplength=100).place(relx=1.0, rely=0.0, anchor="ne").pack(side=tk.TOP, pady=5)
+    dialogue3_display = tk.Label(root, text=dialogue3,width=20,wraplength=100).place(relx=1.0, rely=0.0, anchor="ne").pack(side=tk.TOP, pady=5)
 
 def berry_gather():
-        berries_button.config(state="disabled")
-        berries_button.after(4999, lambda: changeamount("berries", 1))
-        berries_counter.after(5000, lambda: update(berries_counter, science_counter))
-        berries_button.after(5001, lambda: berries_button.config(state="normal"))
-    berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
+    berries_button.config(state="disabled")
+    berries_button.after(4999, lambda: changeamount("berries", 1))
+    berries_counter.after(5000, lambda: update(berries_counter, science_counter))
+    berries_button.after(5001, lambda: berries_button.config(state="normal"))
 
 def brainstorm():
-        brainstorm_button.config(state="disabled")
-        brainstorm_button.after(29999, lambda: changeamount("berries", 1))
-        science_counter.after(30000, lambda: update(berries_counter, science_counter))
-        brainstorm_button.after(30001, lambda: berries_button.config(state="normal"))
+    brainstorm_button.config(state="disabled")
+    brainstorm_button.after(29999, lambda: changeamount("berries", 1))
+    science_counter.after(30000, lambda: update(berries_counter, science_counter))
+    brainstorm_button.after(30001, lambda: berries_button.config(state="normal"))
+
+#Berries
+berries_counter = tk.Label(root, text = "Berries: 0")
+berries_button = tk.Button(root, text="Gather Berries", command=berry_gather)
+brainstorm_button = tk.Button(root, text="Brainstorm", command=brainstorm)
+science_counter = tk.Label(root, text = "Science: 0")
 
 def initialize():
     root = tk.Tk()
@@ -45,12 +50,10 @@ def initialize():
 
     #Berries
     berries_button.place(x=0,y=100)
-    berries_counter = tk.Label(root, text = "Berries: 0")
     berries_counter.place(x=500,y=100)
     #Brainstorm
-    brainstorm_button = tk.Button(root, text="Brainstorm", command=brainstorm)
+    
     brainstorm_button.place(x=0,y=500)
-    science_counter = tk.Label(root, text = "Science: 0")
     science_counter.place(x=100,y=100)
 
     root.after(500,devkey)
