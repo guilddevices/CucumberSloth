488634ec76f16b05829b3461483e188fc4c7e8b0
zacksloth5 <zackgourevitch@gmail.com>
2025-08-06 11:17:54 -0400

    d

--- dialogue.py
+++ dialogue.py
@@ -1,12 +1,19 @@
 dialogue = {
-"Berries": {
+"berries": {
 "5": "You walk around and find some berries.",
 "3": "You pick some berries from a nearby bush.",
 "4": "You gather berries after finding a berry bush behind a tree."
 },
-"Brainstorm": {
+"brainstorm": {
 "1": "You ponder existential questions about life and the universe, and gain Science points from that.",
 "2": "You imagine a place with more resources, and a big city full of people, and realize that you don't want that, and get Science points from that.",
 "3": "You solve an unsolved math eqution using the experiences that you have had in your new Middle of Nowhere experiences, and gain Science points."
+},
+"forage": {
+    "vegetable": "You find some fresh vegetables on the ground but be careful a rabbit is nearby.",
+    "fruit": "You walk right into a patch of freshly grown fruit. You look down in awe at the sweetest sight.",
+    "vegetable_fruit": "You stumbly upon a lucky situation. To you right are ripe fruits, sweet and delicous. To your left you see a vegetable patch, delicacies never to be forgotten. You hastily pick up what you can and come back to camp.",
+    "1": "You slip off of a short branch and fall into a large berry bush. To your suprise it has two unpicked berries.",
+    "2": "You are paying close attention for beasts when you find a berry bush with some berries on it."
 }
 }
