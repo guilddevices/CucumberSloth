8ddcbed37b8b53b9834967f5ad2a6731bbc64133
zacksloth5 <zackgourevitch@gmail.com>
2025-08-06 10:03:24 -0400

    crapincrapout

--- dialogue.py
+++ dialogue.py
@@ -1,12 +1,19 @@
 dialogue = {
-"Berries": {
-"5": "You walk around and find some berries.",
-"3": "You pick some berries from a nearby bush.",
-"4": "You gather berries after finding a berry bush behind a tree."
+"berries": {
+"6": "You walk around and find some berries.",
+"4": "You pick some berries from a nearby bush.",
+"5": "You gather berries after finding a berry bush behind a tree."
 },
-"Brainstorm": {
+"brainstorm": {
 "1": "You ponder existential questions about life and the universe, and gain Science points from that.",
 "2": "You imagine a place with more resources, and a big city full of people, and realize that you don't want that, and get Science points from that.",
 "3": "You solve an unsolved math eqution using the experiences that you have had in your new Middle of Nowhere experiences, and gain Science points."
+},
+"forage": {
+    "vegetable": "You find some vegetables half-eaten by a rabbit on the ground along with your usual surplus of berries.",
+    "fruit": "You find fruit growing out of the ground, in a beautiful sight you collect your sweet treasures.",
+    "vegetable_fruit": "A patch of sweet fruit is growing to your right, and freshly harvested vegetable to your left.",
+    "1": "You wander around until finding a large berry bush. This bush has two berries on it.",
+    "2": "You fall of a tree into a bush. Two berries surround you and you grin with pride."
 }
 }
